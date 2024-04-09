36a5123940b07058c99abde2f4b76e498d83a678
Lap Wang Wong <leonwonglww@gmail.com>
2024-04-09 16:58:41 -0400

    Added breeze auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,15 +8,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+require __DIR__ . '/auth.php';
 // To trash a student
 Route::get(
     'students/trash/{id}',
     [StudentController::class, 'trash']
-)->name('students.trash');
+)->name('students.trash')->middleware(['auth', 'verified']);
 
 // Show all the trashed students
 Route::get(
@@ -29,4 +37,4 @@ Route::get(
 )->name('students.restore');
 
 
-Route::resource('students', StudentController::class);
+Route::resource('students', StudentController::class)->middleware(['auth', 'verified']);
